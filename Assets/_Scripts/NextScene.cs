eadf286b21d23b6074f0bda490cbf1bfc414203f
Tam <tam.tahirli@gmail.com>
2020-12-16 21:09:45 -0500

    Created start scene.  Added image for background, button & button manager. Added menu click sound and other sounds I will add later.

---
+++ Assets/_Scripts/NextScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextScene : MonoBehaviour
+{
+    public Button playBtn;
+    public AudioSource clickSound;
+
+    void Start()
+    {
+        playBtn.GetComponent<Button>().onClick.AddListener(OnClick);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void OnClick()
+    {
+        clickSound.Play();
+        StartCoroutine(LoadNewScene());
+    }
+
+    IEnumerator LoadNewScene()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Main");
+    }
+}
